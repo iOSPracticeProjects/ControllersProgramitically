b32145b037e8d24ba7ef2fee75be888e5d7029fe
Veerababu Mulugu <veeru060786@gmail.com>
2019-02-07 11:52:13 -0700

    Addedf the UiScreen programtically and Added the button programatically.

--- iOSControllers/iOSControllers/AppDelegate.swift
+++ iOSControllers/iOSControllers/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let rootViewController = ViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        
+        
+        
+        
+        
         return true
     }
 
--- iOSControllers/iOSControllers/ViewController.swift
+++ iOSControllers/iOSControllers/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.white
+
+        
+        let myFirstLabel  = UILabel()
+        myFirstLabel.text = "I made a label on the screen #toogood4you"
+        //myFirstLabel.font = UIFont(name: "MarkerFelt-Thin", size: 45)
+        myFirstLabel.textColor = UIColor.blue
+        myFirstLabel.textAlignment = .center
+        myFirstLabel.numberOfLines = 5
+        //myFirstLabel.frame = CGRect(origin: CGPoint(x: 10,y :10), size: CGSize(width: 300, height: 500))
+        
+        //myFirstLabel.frame = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        //myFirstLabel.frame = CGRect(X:15, Y:-50, width: 300, height: 500)
+        
+        myFirstLabel.frame =  CGRect(x: 15, y: 50, width: 300, height: 500)
+        self.view.addSubview(myFirstLabel)
+        
+        
     }
 
 
